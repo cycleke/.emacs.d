bf4bd1914782d98b9bd5e0e0c67f099184e8bdcc
cycleke <cycleke@gmail.com>
2020-08-13 21:25:52 +0800

    修改BUG

--- lisp/init-keybind.el
+++ lisp/init-keybind.el
@@ -18,36 +18,6 @@
 (general-define-key "M-SPC" 'leader-key)
 (bind-key "t p" 'toggle-proxy leader-key)
 
-(use-package hydra
-  :ensure t
-  :commands
-  (hydra-default-pre
-	 hydra-keyboard-quit
-	 hydra--call-interactively-remap-maybe
-	 hydra-show-hint
-	 hydra-set-transient-map))
-
-(use-package major-mode-hydra :ensure t:after hydra)
-
-(use-package pretty-hydra
-  :defines (display-line-numbers-mode linum-mode)
-  :functions set-package-archives
-  :init
-  (cl-defun pretty-hydra-title (title &optional icon-type icon-name
-				                              &key face height v-adjust)
-    "Add an icon in the hydra title."
-    (let ((face (or face `(:foreground ,(face-background 'highlight))))
-	        (height (or height 1.0))
-	        (v-adjust (or v-adjust 0.0)))
-      (concat
-       (when (and (display-graphic-p) icon-type icon-name)
-	       (let ((f (intern (format "all-the-icons-%s" icon-type))))
-	         (when (fboundp f)
-	           (concat
-	            (apply f (list icon-name :face face :height height :v-adjust v-adjust))
-	            " "))))
-       (propertize title 'face face)))))
-
 ;; Global toggles
 (pretty-hydra-define toggles-hydra
   (:title (pretty-hydra-title "Toggles" 'faicon "toggle-on")
--- lisp/init-package.el
+++ lisp/init-package.el
@@ -100,6 +100,37 @@
 (use-package general :defer t)
 (use-package try :ensure t)
 
+(use-package hydra
+  :ensure t
+  :commands
+  (hydra-default-pre
+	 hydra-keyboard-quit
+	 hydra--call-interactively-remap-maybe
+	 hydra-show-hint
+	 hydra-set-transient-map))
+
+(use-package major-mode-hydra :ensure t :after hydra)
+
+(use-package pretty-hydra
+  :ensure t
+  :defines (display-line-numbers-mode linum-mode)
+  :functions set-package-archives
+  :init
+  (cl-defun pretty-hydra-title (title &optional icon-type icon-name
+				                              &key face height v-adjust)
+    "Add an icon in the hydra title."
+    (let ((face (or face `(:foreground ,(face-background 'highlight))))
+	        (height (or height 1.0))
+	        (v-adjust (or v-adjust 0.0)))
+      (concat
+       (when (and (display-graphic-p) icon-type icon-name)
+	       (let ((f (intern (format "all-the-icons-%s" icon-type))))
+	         (when (fboundp f)
+	           (concat
+	            (apply f (list icon-name :face face :height height :v-adjust v-adjust))
+	            " "))))
+       (propertize title 'face face)))))
+
 (provide 'init-package)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
