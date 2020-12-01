4f1110bca8f112ec1b031f92fc0938f8a637dc99
cycleke <cycleke@gmail.com>
2020-12-01 15:15:47 +0800

    修改了 rime 配置

--- lisp/init-rime.el
+++ lisp/init-rime.el
@@ -11,19 +11,22 @@
 (eval-when-compile
   (require 'init-variables))
 
-(push '(progn
-         (use-package rime
-           :custom
-           (default-input-method "rime")
-           :bind
-           (:map rime-mode-map
-                 ("C-`" . 'rime-send-keybinding))
-           :config
-           (setq rime-user-data-dir
-                 (cond (sys/macp "~/Library/Rime")
-                       (sys/linuxp "~/.local/share/fcitx5/rime")
-                       (t "")))))
-      graphic-only-plugins-setting)
+(push
+ '(progn
+    (use-package rime
+      :custom
+      (default-input-method "rime")
+      :bind
+      (:map rime-mode-map
+            ("C-`" . 'rime-send-keybinding))
+      :config
+      (setq rime-posframe-properties
+		        (list :background-color "#333333"
+			            :foreground-color "#dcdccc"
+			            :internal-border-width 10))
+      (setq rime-user-data-dir "~/.emacs.d/pyim/rime"
+            rime-show-candidate 'posframe)))
+    graphic-only-plugins-setting)
 
 (provide 'init-rime)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
