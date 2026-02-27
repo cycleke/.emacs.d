083d4020fd712ea04faf82c1bcf24df32dd82d96
cycleke <21189363+cycleke@users.noreply.github.com>
2026-02-27 22:09:24 +0800

    feat(cc): 智能檢測編譯器並設置編譯命令
    - 根據文件擴展名和系統可用的編譯器智能設置 compile-command：
      - C++ 文件優先使用 clang++，其次 g++
      - C 文件優先使用 clang，其次 gcc
      - 頭文件使用 make -k
    - 添加 C-c C-c 快捷鍵調用 compile 命令。

--- lisp/init-cc.el
+++ lisp/init-cc.el
@@ -5,7 +5,7 @@
 ;;
 ;;; Commentary:
 ;;
-;;  C/C++ 配置
+;; C/C++ 配置
 ;;
 ;;; Code:
 
@@ -14,12 +14,33 @@
   :custom
   (c-ts-mode-indent-offset 2)
   (c-ts-mode-indent-style 'k&r)
+  :bind
+  (:map c-ts-mode-map
+   ("C-c C-c" . compile)
+   :map c++-ts-mode-map
+   ("C-c C-c" . compile))
   :preface
-  (defun cc-ts-setup ()
+  (defun lu--setup-cc-ts ()
     (modify-syntax-entry ?_ "w")
     (treesit-font-lock-recompute-features)
-    (setq-local compile-command "clang++ -Wall -Wextra -std=c++17 "))
-  :hook ((c-ts-mode c++-ts-mode) . cc-ts-setup))
+    (when buffer-file-name
+      (let* ((file (file-name-nondirectory buffer-file-name))
+             (ext (file-name-extension file))
+             (is-hdr (member ext '("h" "hpp" "hxx" "hh")))
+             (is-cpp (member ext '("cpp" "cc" "cxx" "C" "c++")))
+             (compiler (cond
+                        ((and is-cpp (executable-find "clang++")) "clang++")
+                        ((and is-cpp (executable-find "g++")) "g++")
+                        ((executable-find "clang") "clang")
+                        ((executable-find "gcc") "gcc")))
+             (std (if is-cpp "-std=c++17" "-std=c11")))
+        (setq-local compile-command
+                    (cond
+                     (is-hdr "make -k")
+                     (compiler (format "%s -Wall -Wextra %s -g %s"
+                                       compiler std file))
+                     (t "make -k"))))))
+  :hook ((c-ts-mode c++-ts-mode) . lu--setup-cc-ts))
 
 (provide 'init-cc)
 ;;; init-cc.el ends here
